48eddd52b12f8e019e569841ed505034a8ddcc71
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2023-04-19 20:11:02 +0100

    setup config file, sequelize and server index

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    development: {
+      username: 'tpjfxysv',
+      password: 'Ze-260ghKeLBnxNmqcwFjDmeFnA3vDuH',
+      database: 'tpjfxysv',
+    }
+  };
+  
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
