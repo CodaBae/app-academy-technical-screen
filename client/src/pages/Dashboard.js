7d00b2673fd0e19adf8147321cd5e9b50987dfc8
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2023-04-20 16:12:05 +0100

    added editable name

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -113,7 +113,7 @@ function Dashboard() {
                             {task.length > 0 ? task.map((item, index) => (
                                 <div key={index} className=" display-space">
                                     <div>
-                                       <div contentEditable="true" type='text' onMouseLeave={(e) => {
+                                       <div contentEditable="true" type='text' onInput={(e) => {
                                             updateTask({ title: e.currentTarget.textContent }, item.id)
                                         }}>
                                             {item.title} 
